90535de9d2105de26601a8641f43e8eb4e81f1f5
hugosenari <hugosenari@gmail.com>
2022-03-12 02:39:02 -0300

    chore: re index denim_ui_canvas

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''HTML Canvas backend for the denim ui engine'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."denim_ui_canvas-master".url = "path:./master";
+  inputs."denim_ui_canvas-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."denim_ui_canvas-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = ''HTML Canvas backend for the denim ui engine'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-denim_ui_canvas-master.flake = false;
+  inputs.src-denim_ui_canvas-master.owner = "nortero-code";
+  inputs.src-denim_ui_canvas-master.ref   = "refs/heads/master";
+  inputs.src-denim_ui_canvas-master.repo  = "denim-ui-canvas";
+  inputs.src-denim_ui_canvas-master.type  = "github";
+  
+  inputs."denim_ui".dir   = "nimpkgs/d/denim_ui";
+  inputs."denim_ui".owner = "riinr";
+  inputs."denim_ui".ref   = "flake-pinning";
+  inputs."denim_ui".repo  = "flake-nimble";
+  inputs."denim_ui".type  = "github";
+  inputs."denim_ui".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."denim_ui".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-denim_ui_canvas-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-denim_ui_canvas-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
